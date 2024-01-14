153042c8c1b22dbe97c643900f6dbe9bd6a26cbd
duynvm23451 <duynvm1711@gmail.com>
2024-01-14 15:38:57 +0700

    add v-model to input in search form and refractor code

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -1,23 +1,25 @@
 <template>
-  <form class="border-brand-gray-3 flex h-12 w-full items-center rounded-3xl border border-solid">
+  <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3">
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
         <input
+          v-model="role"
           type="text"
           placeholder="Software engineer"
           class="w-full text-lg font-normal focus:outline-none"
         />
       </div>
       <span
-        class="border-brand-gray-3 bg-brand-gray-2 flex h-full items-center border-l border-r px-3"
+        class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
       >
         in
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
         <input
+          v-model="location"
           type="text"
           placeholder="Los Angeles"
           class="w-full text-lg font-normal focus:outline-none"
@@ -29,9 +31,15 @@
 </template>
 
 <script>
-import ActionButton from './ActionButton.vue'
+import ActionButton from '../Shared/ActionButton.vue'
 export default {
   name: 'JobSearchForm',
-  components: { ActionButton }
+  components: { ActionButton },
+  data() {
+    return {
+      role: '',
+      location: ''
+    }
+  }
 }
 </script>
