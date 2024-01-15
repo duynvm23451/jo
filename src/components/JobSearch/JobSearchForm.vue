6ee8b730b91f88adcb1d68cf8fc3b3c2a570559f
duynvm23451 <duynvm1711@gmail.com>
2024-01-15 18:25:27 +0700

    isolate text input

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -4,12 +4,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
       <span
         class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
@@ -18,12 +13,7 @@
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Los Angeles"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
@@ -32,9 +22,10 @@
 
 <script>
 import ActionButton from '../Shared/ActionButton.vue'
+import TextInput from '../Shared/TextInput.vue'
 export default {
   name: 'JobSearchForm',
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data() {
     return {
       role: '',
